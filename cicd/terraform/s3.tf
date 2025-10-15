fd3a07be9a8c066c165a10b7f1e0dca7d0fadde2
nealio1000 <nealiof1000@gmail.com>
2025-10-15 17:26:33 -0400

    clean up

--- cicd/terraform/s3.tf
+++ cicd/terraform/s3.tf
@@ -1,14 +1,7 @@
-# Define the AWS provider
-provider "aws" {
-  region = "us-east-1"
-}
-
-# Define the S3 bucket resource
 resource "aws_s3_bucket" "rearc_data_bucket" {
   bucket = "rearc-quest-data-bucket"
   acl    = "private"                     
 
-  # Enable versioning for the bucket
   versioning {
     enabled = true
   }
