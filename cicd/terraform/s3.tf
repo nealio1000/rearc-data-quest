590b8e506d0c58308a2046068c55f9862ee5aef8
nealio1000 <nealiof1000@gmail.com>
2025-10-15 17:47:24 -0400

    s3 cleanup

--- cicd/terraform/s3.tf
+++ cicd/terraform/s3.tf
@@ -1,26 +1,9 @@
 resource "aws_s3_bucket" "rearc_data_bucket" {
   bucket = "rearc-quest-data-bucket"
-  acl    = "private"                     
-
-  versioning {
-    enabled = true
-  }
 
   tags = {
     Environment = "Development"
     Project     = "RearcDataQuest"
-  }
-}
-
-resource "aws_s3_bucket_acl" "rearc_data_bucket_acl" {
-  bucket = aws_s3_bucket.rearc_data_bucket.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_versioning" "rearc_data_bucket_versioning" {
-  bucket = aws_s3_bucket.rearc_data_bucket.id
-  versioning_configuration {
-    status = "Enabled"
   }
 }
 
