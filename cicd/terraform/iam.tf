c34d4eb74166c61f7299321a5c854131e434aaa9
nealio1000 <nealiof1000@gmail.com>
2025-10-16 13:11:15 -0400

    working on lambda deployment

---
+++ cicd/terraform/iam.tf
@@ -0,0 +1,67 @@
+# Create an IAM role for the Lambda function
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+    }],
+  })
+}
+
+data "aws_iam_policy_document" "lambda_policy_doc" {
+  statement {
+    sid = "AllowInvokingLambdas"
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:lambda:*:*:function:*"
+    ]
+
+    actions = [
+      "lambda:InvokeFunction"
+    ]
+  }
+
+  statement {
+    sid = "AllowCreatingLogGroups"
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:logs:*:*:*"
+    ]
+
+    actions = [
+      "logs:CreateLogGroup"
+    ]
+  }
+
+  statement {
+    sid = "AllowWritingLogs"
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:logs:*:*:log-group:/aws/lambda/*:*"
+    ]
+
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "lambda_iam_policy" {
+  name = "lambda_iam_policy"
+  policy = data.aws_iam_policy_document.lambda_policy_doc.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  policy_arn = aws_iam_policy.lambda_iam_policy.arn
+  role = aws_iam_role.lambda_exec_role.name
+}
--- cicd/terraform/lambda.tf
+++ cicd/terraform/lambda.tf
@@ -1,19 +1,3 @@
-# Create an IAM role for the Lambda function
-resource "aws_iam_role" "lambda_exec_role" {
-  name = "lambda_execution_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      },
-    }],
-  })
-}
-
 # Attach a policy to the IAM role for basic Lambda execution
 resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
   role       = aws_iam_role.lambda_exec_role.name
