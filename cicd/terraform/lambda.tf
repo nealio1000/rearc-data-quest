e684ba35012f5e299c12c2d518f00bdb228ad182
nealio1000 <nealiof1000@gmail.com>
2025-10-16 15:35:20 -0400

    clean up warning

--- cicd/terraform/lambda.tf
+++ cicd/terraform/lambda.tf
@@ -38,7 +38,7 @@ resource "null_resource" "install_python_dependencies" {
 }
 
 data "archive_file" "create_dist_pkg" {
-  depends_on = ["null_resource.install_python_dependencies"]
+  depends_on = [null_resource.install_python_dependencies]
   source_dir = "${path.cwd}/lambda_dist_pkg/"
   output_path = var.output_path
   type = "zip"
