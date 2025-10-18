b0a795bc4f25e9f2c4c59147f570129d8976ee64
Neal Friedman <nealiof1000@gmail.com>
2025-10-18 13:54:33 -0400

    setting up tests

--- lambdas/tests/test_rearc_fetcher.py
+++ lambdas/tests/test_rearc_fetcher.py
@@ -1,5 +1,7 @@
 import pytest
 from src.rearc_fetcher import sanitize_data
+from src.s3_utils import synchronize_with_s3
+from src.file_downloader import download_bls_data, download_population_data
 
 def test_sanitize():
     assert sanitize_data() == "sanitized"
