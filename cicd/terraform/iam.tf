ad712c251ab207d0596f5e0bb9077b1d7482c2f8
Neal Friedman <nealiof1000@gmail.com>
2025-10-18 15:16:36 -0400

    add spark job assembly to github actions,  adding iam for emr serverless, preparing spark job

--- cicd/terraform/iam.tf
+++ cicd/terraform/iam.tf
@@ -56,14 +56,17 @@ data "aws_iam_policy_document" "lambda_policy_doc" {
   }
 
   statement {
-    sid = "S3Put"
+    sid = "S3Write"
     effect = "Allow"
     resources = [
-      "${aws_s3_bucket.rearc_data_bucket.arn}/*",
-      "${aws_s3_bucket.rearc_data_bucket.arn}" 
+      "${aws_s3_bucket.rearc_data_bucket.arn}/population",
+      "${aws_s3_bucket.rearc_data_bucket.arn}/population/*",
+      "${aws_s3_bucket.rearc_data_bucket.arn}/bls-data",
+      "${aws_s3_bucket.rearc_data_bucket.arn}/bls-data/*",
     ]
     actions = [
-      "s3:PutObject"
+      "s3:PutObject",
+      "s3:ListBucket"
     ]
   }
 }
@@ -76,4 +79,43 @@ resource "aws_iam_policy" "lambda_iam_policy" {
 resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
   policy_arn = aws_iam_policy.lambda_iam_policy.arn
   role = aws_iam_role.lambda_exec_role.name
+}
+
+data "aws_iam_policy_document" "emr_serverless_policy_doc" {
+  statement {
+    sid = "S3ReadWrite"
+    effect = "Allow"
+    resources = [
+      "${aws_s3_bucket.rearc_data_bucket.arn}/population",
+      "${aws_s3_bucket.rearc_data_bucket.arn}/population/*",
+      "${aws_s3_bucket.rearc_data_bucket.arn}/bls-data",
+      "${aws_s3_bucket.rearc_data_bucket.arn}/bls-data/*",
+    ]
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:PutObject"
+    ]
+  }
+}
+
+resource "aws_iam_role" "emr_serverless_job_role" {
+  name = "emr-serverless-job-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "emr-serverless.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "s3_access" {
+  role       = aws_iam_role.emr_serverless_job_role.name
+  policy_arn = data.aws_iam_policy_document.emr_serverless_policy_doc.json
 }
