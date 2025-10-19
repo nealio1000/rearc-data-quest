3901933ee4314e5a000501ed9e5b591e65097f93
Neal Friedman <nealiof1000@gmail.com>
2025-10-18 23:30:13 -0400

    python packaging

--- lambdas/src/test_rearc_fetcher.py
+++ lambdas/src/test_rearc_fetcher.py
@@ -1,7 +1,5 @@
 import pytest
 from rearc_fetcher import sanitize_data
-from s3_utils import synchronize_with_s3
-from file_downloader import download_bls_data, download_population_data
 
 def test_sanitize():
     assert sanitize_data() == "sanitized"
