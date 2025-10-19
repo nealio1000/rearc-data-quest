fcfa666b59651bc9e64193f0eaceb206bad74fa2
Neal Friedman <nealiof1000@gmail.com>
2025-10-19 00:47:03 -0400

    add in eventbridge scheduling

---
+++ cicd/terraform/eventbridge.tf
@@ -0,0 +1,16 @@
+# EventBridge Scheduler to run Step Function daily
+resource "aws_scheduler_schedule" "daily_sfn_schedule" {
+  name = "daily-sfn-trigger"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  schedule_expression = "cron(0 0 * * ? *)" # Runs daily at midnight UTC
+
+  target {
+    arn      = aws_sfn_state_machine.daily_fetch_data_state_machine.arn
+    role_arn = aws_iam_role.eventbridge_scheduler_role.arn
+    input    = jsonencode({"message": "Triggered by daily schedule"})
+  }
+}
