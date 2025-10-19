db971ec0c365498ae5c8d332973e3f0b3997f72d
Neal Friedman <nealiof1000@gmail.com>
2025-10-19 10:29:20 -0400

    emr serverless role needs delete to overwrite files in s3

--- cicd/terraform/iam.tf
+++ cicd/terraform/iam.tf
@@ -134,7 +134,8 @@ data "aws_iam_policy_document" "emr_serverless_policy_doc" {
     actions = [
       "s3:GetObject",
       "s3:ListBucket",
-      "s3:PutObject"
+      "s3:PutObject",
+      "s3:DeleteObject"
     ]
   }
 
